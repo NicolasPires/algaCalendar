e74c9d1581f28ad7d89320ad15eb149533ee7af5
nicolas.pires <nicolask.pires@gmail.com>
2023-09-06 14:30:09 -0300

    Adicionando api contract first no repositório

---
+++ src/main/java/com/example/algaworks/calendar/domain/ReminderSpecs.java
@@ -0,0 +1,27 @@
+package com.example.algaworks.calendar.domain;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+public class ReminderSpecs {
+    public static Specification<Reminder> usingFilter(ReminderFilter filter) {
+        return (root, query, builder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (Objects.isNull(filter)) {
+                return builder.and(predicates.toArray(new Predicate[0]));
+            }
+
+            if (StringUtils.hasText(filter.getTitle())) {
+                predicates.add(builder.equal(root.get("title"), filter.getTitle()));
+            }
+            return builder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+}
